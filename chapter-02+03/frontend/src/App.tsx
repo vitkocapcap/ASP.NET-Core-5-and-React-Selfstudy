beea5d804b85d7e78cadd0096e226ffb94507459
Microsoft Windows <Microsoft Windows@BAOVIET>
2021-03-05 19:51:08 +0100

    merge chap3 to chap2

---
+++ chapter-02+03/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+
+function App() {
+  const unused = 'something';
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ chapter-02+03/frontend/src/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export const HomePage = () => (
+  <div>
+    <div>
+      <h2>Unanswered Questions</h2>
+      <button>Ask a question</button>
+    </div>
+  </div>
+);
