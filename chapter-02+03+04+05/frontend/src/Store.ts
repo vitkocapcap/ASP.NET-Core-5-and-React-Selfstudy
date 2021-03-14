a9d958b34c310e2048a16e9e411c99f8b9c1bd76
vitkocapcap <viet.npb2212@gmail.com>
2021-03-14 18:14:35 +0100

    start with redux

---
+++ chapter-02+03+04+05/frontend/src/Store.ts
@@ -0,0 +1,17 @@
+import { QuestionData } from './QuestionsData';
+interface QuestionsState {
+  readonly loading: boolean;
+  readonly unanswered: QuestionData[];
+  readonly viewing: QuestionData | null;
+  readonly searched: QuestionData[];
+}
+export interface AppState {
+  readonly questions: QuestionsState;
+}
+
+const initialQuestionState: QuestionsState = {
+  loading: false,
+  unanswered: [],
+  viewing: null,
+  searched: [],
+};
