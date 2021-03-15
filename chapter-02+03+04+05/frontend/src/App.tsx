073f189897b30afa37ccd69569c46b136de49b37
vitkocapcap <viet.npb2212@gmail.com>
2021-03-15 12:57:17 +0100

    update redux proviver and dispatch

--- chapter-02+03+04+05/frontend/src/App.tsx
+++ chapter-02+03+04+05/frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { fontFamily, fontSize, gray2 } from './Styles';
 
 //Components
 import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from './Store';
 import './App.css';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
@@ -17,46 +19,49 @@ import { PageNotFound } from './PageNotFound';
 import { QuestionPage } from './QuestionPage';
 
 const AskPage = React.lazy(() => import('./AskPage'));
+const store = configureStore();
 
 function App() {
   return (
-    <BrowserRouter>
-      <div
-        css={css`
-          font-family: ${fontFamily};
-          font-size: ${fontSize};
-          color: ${gray2};
-        `}
-      >
-        <Header />
-        <Routes>
-          <Route path="" element={<HomePage />} />
-          <Route path="search" element={<SearchPage />} />
-          <Route
-            path="ask"
-            element={
-              <React.Suspense
-                fallback={
-                  <div
-                    css={css`
-                      margin-top: 100px;
-                      text-align: center;
-                    `}
-                  >
-                    Loading...
-                  </div>
-                }
-              >
-                <AskPage />
-              </React.Suspense>
-            }
-          />
-          <Route path="signin" element={<SignInPage />} />
-          <Route path="questions/:questionId" element={<QuestionPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div
+          css={css`
+            font-family: ${fontFamily};
+            font-size: ${fontSize};
+            color: ${gray2};
+          `}
+        >
+          <Header />
+          <Routes>
+            <Route path="" element={<HomePage />} />
+            <Route path="search" element={<SearchPage />} />
+            <Route
+              path="ask"
+              element={
+                <React.Suspense
+                  fallback={
+                    <div
+                      css={css`
+                        margin-top: 100px;
+                        text-align: center;
+                      `}
+                    >
+                      Loading...
+                    </div>
+                  }
+                >
+                  <AskPage />
+                </React.Suspense>
+              }
+            />
+            <Route path="signin" element={<SignInPage />} />
+            <Route path="questions/:questionId" element={<QuestionPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
